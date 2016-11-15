6215100e697a5b5da2635bd39ffd13cb04bcac00
BuildTools <unconfigured@null.spigotmc.org>
2016-11-14 20:59:13 -0600

    Fix errors with tank drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Driver12285.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Driver12285.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name="Custom: 12285 Driver", group="Linear Opmode")
 public class Driver12285 extends LinearOpMode {
@@ -22,15 +21,8 @@ public class Driver12285 extends LinearOpMode {
         waitForStart(); // Wait for the driver to press play
 
         while (opModeIsActive()) {
-            // this is now tank FUCKING DRIVE
-            right  = -gamepad1.right_stick_y;
             left = -gamepad1.left_stick_y;
-
-            right = Range.clip(right, -1, 1);
-            left = Range.clip(left, -1, 1);
-
-            right = (float) scaleInput(right);
-            left = (float) scaleInput(left);
+            right  = -gamepad1.right_stick_y;
 
             robot.getLeftWheel().setPower(left);
             robot.getRightWheel().setPower(right);
@@ -53,30 +45,6 @@ public class Driver12285 extends LinearOpMode {
 
             robot.waitForTick(40);
         }
-    }
-
-    private double scaleInput(double dVal) {
-        double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24, 0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00 };
-
-        int index = (int)(dVal * 16.0);
-
-        if (index < 0) {
-            index = -index;
-        }
-
-        if (index > 16) {
-            index = 16;
-        }
-
-        double dScale = 0.0;
-
-        if (dVal < 0) {
-            dScale = -scaleArray[index];
-        } else {
-            dScale = scaleArray[index];
-        }
-
-        return dScale;
     }
 
 }
