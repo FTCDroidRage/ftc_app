90685ba9a01d4adfd1e3235ea388c8ac003b9758
Ricky Tiet <Ricky Tiet>
2017-01-21 13:52:27 -0600

    Add versions of both autonomous modes to change the timing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Drive2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Drive2.java
@@ -20,7 +20,7 @@ public class Drive2 extends LinearOpMode {
 
         waitForStart();
 
-        double forwardSpeed = -0.5;
+        double forwardSpeed = 0.5;
 
         robot.getWheelLeft().setPower(-forwardSpeed);
         robot.getWheelRight().setPower(forwardSpeed);
